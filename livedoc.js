507fbfef59db568b44e3cac4d4f2f6278ac1c541
Mark Cipolla <mark@markcipolla.com>
2012-05-11 13:27:11 +1000

    Version Bump. 0.0.2 - Working, steps in, colours there... Yeah

--- livedoc.js
+++ livedoc.js
@@ -1,7 +1,7 @@
 // LiveDoc - Check specific page level tags to generate in-page, live documentation.
 // by Mark Cipolla, http://www.markcipolla.com
 
-// Version 0.0.1
+// Version 0.0.2
 // Full source at https://github.com/markcipolla/LiveDoc
 // Copyright (c) 2012 Mark Cipolla
 
@@ -26,8 +26,6 @@ var LiveDoc = (function () {
             for (i = 0; i < features.length; i++) {
 
                 var feature = features[i];
-                console.log(feature);
-
                 var feature_tags = $.map( feature["tags"], function(val, i) {
                     return val.name;
                 });
@@ -35,13 +33,39 @@ var LiveDoc = (function () {
                 if( $.inArray(tags, feature_tags) != -1 ) {
 
                     $("#livedoc_results").append("<div class='livedoc_result'></div>");
-                    $("#livedoc_results .livedoc_result").last().html("<h4>" + feature["keyword"] + " - " + feature["name"]) + "</h4>";
+                    var result = $("#livedoc_results .livedoc_result").last();
+                    result.html("<h4>" + feature["keyword"] + " - " + feature["name"]) + "</h4>";
+
+                    // Add a 'pending' class if the feature is @wip
+                    if( $.inArray("@wip", feature_tags) != -1 ) {
+                        $("#livedoc_results .livedoc_result").last().addClass("pending");
+                    }
 
                     if ( feature["elements"]) {
-                      for (j = 0; j < feature["elements"].length; j++) {
-                        var scenario = feature["elements"][j];
-                        console.log(scenario);
-                      }
+
+                        for (j = 0; j < feature["elements"].length; j++) {
+                            // Get the Scenario
+                            var scenario = feature["elements"][j];
+                            console.log(scenario);
+
+                            var scenario_tags = $.map( scenario["tags"], function(val, i) {
+                                return val.name;
+                            });
+
+                            if( $.inArray(tags, scenario_tags) != -1 ) {
+                                // Append the Scenario details
+                                result.append("<dl><dt><span>" + scenario.type + "</span>" + scenario.name + "</dt><dd></dd></dl>")
+                                result.find("dd").last().append("<ul></ul>");
+
+                                var steps_list = $(result).find("ul").last();
+                                if ( scenario["steps"]) {
+                                    for (k = 0; k < scenario["steps"].length; k++) {
+                                        // Append the Step details
+                                        steps_list.append("<li class='" + scenario["steps"][k]["result"]["status"] + "'>" + scenario["steps"][k]["keyword"] + " " + scenario["steps"][k]["name"] + "</li>");
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
